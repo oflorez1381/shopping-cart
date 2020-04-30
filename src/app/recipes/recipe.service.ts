01d65eca1837aa2a0d5d2eb20f2eec394a83ed8a
oflorez <v-oflorez@expedia.com>
2020-04-29 20:29:03 -0400

    10.7 Adding Ingredients to Recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,28 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService {
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe[] = [
-    new Recipe('A Test', 'This is simply a test',
+    new Recipe('A Test',
+      'This is simply a test',
       // tslint:disable-next-line:max-line-length
-      'https://images.unsplash.com/photo-1548946522-4a313e8972a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1340&q=80'),
-    new Recipe('An Another Test', 'This is simply a test',
-      'https://images.unsplash.com/photo-1548946522-4a313e8972a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1340&q=80')
+      'https://images.unsplash.com/photo-1548946522-4a313e8972a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1340&q=80',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 10)
+      ]),
+    new Recipe('An Another Test',
+      'This is simply a test',
+      // tslint:disable-next-line:max-line-length
+      'https://images.unsplash.com/photo-1548946522-4a313e8972a4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1340&q=80',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ])
   ];
 
   getRecipes(): Recipe[] {
